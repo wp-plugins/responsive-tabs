b5202ed13c6e6bc167caac6da81b0c3f4d008292
spwebguy <spwebguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 09:34:59 +0000

    Version 3.1 — minified
    git-svn-id: https://plugins.svn.wordpress.org/responsive-tabs/trunk@1219792 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rtbs.php
+++ rtbs.php
@@ -93,8 +93,8 @@ function rtbs_old_data() {
 /* Enqueue scripts & styles */
 add_action( 'wp_enqueue_scripts', 'add_rtbs_scripts', 99 );
 function add_rtbs_scripts() {
-	wp_enqueue_style( 'rtbs', plugins_url('css/rtbs_style.css', __FILE__));
-    wp_enqueue_script( 'rtbs', plugins_url('js/rtbs.js', __FILE__), array( 'jquery' ));
+	wp_enqueue_style( 'rtbs', plugins_url('css/rtbs_style.min.css', __FILE__));
+    wp_enqueue_script( 'rtbs', plugins_url('js/rtbs.min.js', __FILE__), array( 'jquery' ));
 }
 
 
